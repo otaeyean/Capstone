3636c77e0f32117cfd8e587df1a6af9520f6ec92
otaeyean <oty12125@naver.com>
2025-03-02 12:59:47 +0900

    update: 이동평균선, 확대축소, ui 수정

--- lib/investment/chart/stock_price.dart
+++ lib/investment/chart/stock_price.dart
@@ -25,4 +25,16 @@ class StockPrice {
       volume: json['volume'],
     );
   }
+
+  // ✅ 1분봉 데이터 변환을 위한 팩토리 추가
+  factory StockPrice.fromJsonMinute(Map<String, dynamic> json) {
+    return StockPrice(
+      date: DateTime.parse("${json['date']} ${json['time']}"),  // ✅ 날짜+시간 변환
+      open: json['openPrice'].toDouble(),
+      high: json['highPrice'].toDouble(),
+      low: json['lowPrice'].toDouble(),
+      close: json['closePrice'].toDouble(),
+      volume: json['volume'],
+    );
+  }
 }
